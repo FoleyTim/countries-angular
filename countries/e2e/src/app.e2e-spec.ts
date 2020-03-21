2c3eca2bbf2397045c79b639ff8e6d14c52c6b52
Tim <tim.foley@contactenergy.co.nz>
2020-03-21 14:22:17 +0000

    added e2e tests

--- countries/e2e/src/app.e2e-spec.ts
+++ countries/e2e/src/app.e2e-spec.ts
@@ -8,10 +8,29 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('countries app is running!');
+    expect(page.getTitleText()).toEqual('Countries');
   });
+
+  it('should display region dropdown', () => {
+    page.navigateTo();
+    expect(page.getRegionsDropDownLabel()).toEqual('Region:');
+  });
+
+  it('should display countries dropdown', () => {
+    page.navigateTo();
+    page.getRegionsOption().click();
+    expect(page.getRegionsCountriesLabel()).toEqual('Country:');
+  });
+
+  it('should display country details table', () => {
+    page.navigateTo();
+    page.getRegionsOption().click();
+    page.getCountriesOption().click();
+    expect(page.getCountryDetailsTableCountryName()).toEqual('Afghanistan');
+  });
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- countries/e2e/src/app.po.ts
+++ countries/e2e/src/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, WebElement, WebElementPromise } from 'protractor';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
@@ -6,6 +6,25 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('app-root .toolbar span')).getText() as Promise<string>;
   }
+
+  getRegionsDropDownLabel(): Promise<string> {
+    return element(by.css('app-root .content .dropdown-bar app-dropdown .drop-down-container label')).getText() as Promise<string>;
+  }
+
+  getRegionsOption() {
+    return element(by.css('app-root .content .dropdown-bar #region-drop-down  div  select  option:nth-child(2)'));
+  }
+  getRegionsCountriesLabel(): Promise<string> {
+    return element(by.css('app-root .content .dropdown-bar #countries-drop-down .drop-down-container label')).getText() as Promise<string>;
+  }
+  getCountriesOption() {
+    return element(by.css('app-root .content .dropdown-bar  #countries-drop-down div  select  option:nth-child(2)'));
+  }
+
+  getCountryDetailsTableCountryName(): Promise<string>{
+    return element(by.css('app-root .content  app-country-details table  tr:nth-child(2)  td:nth-child(1)')).getText() as Promise<string>;
+  }
+
 }
